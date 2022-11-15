ab2c80f4e7d6321b66d17dd2a44be740dcfc9f60
Akshay Nair <phenax5@gmail.com>
2022-11-15 12:58:33 +0530

    animates rendering points on canvas

--- script.js
+++ script.js
@@ -1,11 +1,9 @@
 const $canvas = Object.assign(document.createElement("canvas"), {
-  width: 600,
-  height: 600,
-  style: "border: 1px solid white;",
+  width: 1200,
+  height: 675,
 });
 document.body.appendChild($canvas);
 const ctx = $canvas.getContext("2d");
-const imageData = ctx.getImageData(0, 0, $canvas.width, $canvas.height);
 
 const C = {
   new: (x, y) => ({ x, y }),
@@ -18,51 +16,65 @@ const offset = {
   y: $canvas.height / 2,
 };
 
-const scale = {
-  x: $canvas.width / 3,
-  y: $canvas.height / 3,
-};
+const scale = Math.min($canvas.width, $canvas.height) / 2
 
-const MAX_ITERATIONS = 20;
+const MAX_ITERATIONS = 10000;
 
 const alphaChannelMap = new Map
 const getAlpha = (x, y) => alphaChannelMap.get(`${x}--${y}`) ?? 0
 const setAlpha = (x, y, a) => alphaChannelMap.set(`${x}--${y}`, a)
 
-for (let i = 0; i < imageData.data.length; i += 4) {
-  const index = i / 4;
-  const col = index % $canvas.width;
-  const row = Math.floor(index / $canvas.height);
+const nextFrame = () => new Promise(res => requestAnimationFrame(res))
 
-  const c = C.new((col - offset.x) / scale.x, (row - offset.y) / scale.y);
+const run = async () => {
+  let pointsDrawn = 0
 
-  let steps = 0;
-  let z = C.new(0, 0);
+  const cols = $canvas.width*5/4
+  const rows = $canvas.width*5/4
 
-  let escapes = false;
-  const zList = [];
-  for (steps = 0; steps < MAX_ITERATIONS; steps++) {
-    z = C.add(C.square(z), c);
-    zList.push(z);
+  for (let row = -100; row < rows; row++) {
+    for (let col = -100; col < cols; col++) {
+      const c = C.new((col - offset.x) / scale, (row - offset.y) / scale);
 
-    const boundary = 2;
-    if (Math.abs(z.x) > boundary || Math.abs(z.y) > boundary) {
-      escapes = true;
-      break;
-    }
-  }
+      let steps = 0;
+      let z = C.new(0, 0);
 
-  if (escapes) {
-    for (const z of zList) {
-      const row = Math.floor(z.y * scale.y + offset.y);
-      const col = Math.floor(z.x * scale.x + offset.x);
+      let escapes = false;
+      const zList = [];
+      for (steps = 0; steps < MAX_ITERATIONS; steps++) {
+        z = C.add(C.square(z), c);
+        zList.push(z);
 
-      if (row > 0 && col > 0 && row < $canvas.height && col < $canvas.width) {
-        const alpha = getAlpha(col, row) + 0.1
-        ctx.fillStyle = `rgba(200, 10, 0, ${alpha})`
-        ctx.fillRect(col, row, 1, 1)
-        setAlpha(row, col, alpha)
+        if (Math.abs(z.x) > 2 || Math.abs(z.y) > 2) {
+          escapes = true;
+          break;
+        }
+      }
+
+      if (escapes) {
+        for (const [i, z] of zList.entries()) {
+          const y = Math.floor(z.y * scale + offset.y);
+          const x = Math.floor(z.x * scale + offset.x);
+
+          if (y > 0 && x > 0 && y < $canvas.height && x < $canvas.width) {
+            const alpha = Math.min(1, getAlpha(x, y) * 0.2 + 0.1)
+            const [r, g, b] =
+              i > MAX_ITERATIONS / 5 ? [200, 10, 10]
+                : i > MAX_ITERATIONS / 10 ? [200, 10, 100]
+                  : [10, 10, 200]
+            ctx.fillStyle = `rgba(${r}, ${g}, ${b}, ${alpha})`
+            ctx.fillRect(x, y, 1, 1)
+            setAlpha(y, x, alpha)
+          }
+        }
+      }
+
+      pointsDrawn++
+      if (pointsDrawn % 1000 === 0) {
+        await nextFrame()
       }
     }
   }
 }
+
+run()
